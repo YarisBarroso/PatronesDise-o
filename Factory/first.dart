53e342fa707d69b13acdd55124f089e9a8819e88
YarisBarroso <126476940+YarisBarroso@users.noreply.github.com>
2024-05-08 09:46:12 -0500

    Update first.dart

--- Factory/first.dart
+++ Factory/first.dart
@@ -23,7 +23,7 @@ class Generica implements Bicicleta {
 class Infantil implements Bicicleta{
   @override
   void pedaliar(){
-    print(' Bicicleta infantil en funcion ');
+    print(' Bicicleta infantil en funcion ') ;
   }
 }
 
