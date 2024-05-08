4b6b796f08c1e703ea80ef1a34b197771048f75e
YarisBarroso <126476940+YarisBarroso@users.noreply.github.com>
2024-05-08 10:09:55 -0500

    Update ej2.dart

--- Factory/ej2.dart
+++ Factory/ej2.dart
@@ -39,6 +39,6 @@ class Generica implements Bicicleta {
 class Infantil implements Bicicleta {
   @override
   void pedaliar() {
-    print(' Bicicleta infantil en funcion');
+    print(' Bicicleta infantil en funcion ');
   }
 }
